d459e85627651dedce39b8e8f807450ade283947
Kevin <stoutkev3@gmail.com>
2017-12-02 01:08:16 -0500

    added basic layout to the project

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test',
+    'http://maxpixel.freegreatpicture.com/static/photo/1x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg'),
+    new Recipe('hello', 'testing stuff', 'https://www.rd.com/wp-content/uploads/2016/02/06-train-cat-shake-hands.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
